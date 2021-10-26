d9c9b591a214269052ae80f78774a5c25e37e4ad
Michael Rider <mikepdxrider@gmail.com>
2021-10-26 09:39:43 -0700

    adds passing fetchQuotes test with mock fetch

--- __tests__/fetch-quotes-utils.test.js
+++ __tests__/fetch-quotes-utils.test.js
@@ -1,8 +1,28 @@
+
+const { default: fetch } = require('cross-fetch');
 const fetchQuotes = require('../src/fetch-quotes-utils.js');
 
+jest.mock('cross-fetch');
 
-describe('fetchQuotes function returns a an object containing name, quote, and image properties from an api.', () =>  {
+describe('fetchQuotes function returns an object containing name, quote, and image properties from an api.', () =>  {
   it('fetchQuotes returns expected object format.', async () => {
+    
+
+    // This was really hard and confusing. When we're learning new things it's hard to remember and trust fundamental knowledge because we aren't confident in the new environment or how it's variables might affect our existing truths.
+    // Our fetch response must be an object with a json method. 
+    // This is a fact because in fetchQuotes() `response.json()` implies that response is an object, and json() is a method. Thus, when we are mocking fetch, we must mock this method. Huge shoutout to Erich for helping me see this!
+    const mockResponse = {
+      json: () => [
+        {
+          'character': 'Fry',
+          'quote': 'In my time we didn\'t depend on high-tech gadgets like you do. We didn\'t need\na mechanical washing unit to wash our clothes, we just used a washing machine!',
+          'image': 'https://res.cloudinary.com/dzxqhkyqd/image/upload/v1554904133/fry.png'
+        }
+      ]
+    };
+
+    fetch.mockResolvedValue(mockResponse);
+    
     const expectedObj = {
       name: expect.any(String),
       quote: expect.any(String),
