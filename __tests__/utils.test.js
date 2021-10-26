fcd68d1ec8b7ca8f0f2c68bd4f9f4c49c055c371
Michael Rider <mikepdxrider@gmail.com>
2021-10-25 17:05:17 -0700

    adds getName test fail

---
+++ __tests__/utils.test.js
@@ -0,0 +1,9 @@
+
+describe('Red, Green, Refactor Lab', () =>  {
+  it('getName returns the name property of an object', () => {
+    const nameStr = 'dan';
+
+    expect(nameStr).toEqual('stan');
+  });
+});
+
